5e0313caea2c966f83e96d9551b1ca58f2615005
LittleLittleStory <36630033+LittleLittleStory@users.noreply.github.com>
2020-07-15 19:40:50 +0800

    第七节8：48

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,11 +1,39 @@
 # include <Windows.h>
+# include "WindowsMessageMap.h"
+# include <sstream>
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	static WindowsMessageMap mm;
+	OutputDebugString(mm(msg, lParam, wParam).c_str());
 	switch (msg)
 	{
 		case WM_CLOSE:
-		PostQuitMessage(69);
+			PostQuitMessage(69);
+			break;
+
+		case  WM_KEYDOWN:
+			if (wParam == 'F')
+			{
+				SetWindowText(hWnd, "Respects");
+			}
+			break;
+
+		case WM_CHAR:
+		{
+			static std::string title;
+			title.push_back((char)wParam);
+			SetWindowText(hWnd, title.c_str());
+		}
+		break;
+
+		case WM_LBUTTONDOWN:
+		{
+			const POINTS pt = MAKEPOINTS(lParam);
+			std::ostringstream oss;
+			oss << "(" << pt.x << "," << pt.y << ")";
+			SetWindowText(hWnd, oss.str().c_str());
+		}
 		break;
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
@@ -49,7 +77,7 @@ int CALLBACK WinMain(
 	BOOL gResult;
 	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
 	{
-		TranslateMessage(&msg);
+		//TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 	if (gResult == -1)
---
+++ hw3d/Window.cpp
@@ -0,0 +1,45 @@
+#include "Window.h"
+
+Window::WindowClass Window::WindowClass::wndClass;
+
+Window:: WindowClass ::WindowClass() noexcept: hInst(GetModuleHandle(nullptr))
+{
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = HandleMsgSetup;
+	wc.cbClsExtra = 0;
+	wc.cbClsExtra = 0;
+	wc.hInstance = GetInstance();
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = GetName();
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+}
+
+Window::Window(int width, int height, const char* name)noexcept
+{
+	RECT wr;
+	wr.left = 100;
+	wr.right = width + wr.left;
+	wr.top = 100;
+	wr.bottom = height + wr.top;
+	AdjustWindowRect(&wr, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, false);
+	//Create & get hWnd
+	hWnd = CreateWindow(
+		WindowClass::GetName(),name,
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		CW_USEDEFAULT,CW_USEDEFAULT,wr.right-wr.left,wr.bottom-wr.top,
+		nullptr,nullptr,WindowClass::GetInstance(),this
+	);
+	//Show
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+}
+
+Window::~Window()
+{
+	DestoryWindow();
+}
